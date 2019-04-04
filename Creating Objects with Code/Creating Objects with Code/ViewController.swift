d9c471b3fed2f7acce0cfbeddf1dae8485918a7c
Peter Jenkin <peternjenkin@gmail.com>
2019-04-04 12:50:23 +0100

    5-33 Programmatic placement of objects/controls with relative coordinates

--- Creating Objects with Code/Creating Objects with Code/ViewController.swift
+++ Creating Objects with Code/Creating Objects with Code/ViewController.swift
@@ -13,12 +13,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // default action on loading
+        
+        
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        
+        let myLabel = UILabel()     // let not var as Label will not be changed to another type (immutable) https://stackoverflow.com/a/24002209   https://stackoverflow.com/a/31651715
+        myLabel.text = "My code-written label"
+        myLabel.textAlignment = NSTextAlignment.center      // key-in '.' to get options
+        myLabel.textAlignment = .center
+        //myLabel.frame = CGRect(x:10, y:10, width: 100, height:50)
+        myLabel.frame = CGRect(x:width * 0.1, y: height * 0.3, width: width * 0.8, height:50)
+        // NB relative coordinates
+        view.addSubview(myLabel)
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
